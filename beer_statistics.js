528d2703c33695a0e37c2fb35b73be099e90e42f
Jon Packer <contact@jonpacker.com>
2018-05-05 10:38:39 +0200

    sort descending

--- beer_statistics.js
+++ beer_statistics.js
@@ -44,7 +44,7 @@ exports.getBayesianTopRated = async (db, venueId, fetchCount, sesspfx, sortMode)
     );
   } else {
     topRatedBeers.sort(
-      firstBy('grossCheckinCount')
+      firstBy('grossCheckinCount', -1)
       .thenBy(b => b.beer.beer_name)
     );
   }
